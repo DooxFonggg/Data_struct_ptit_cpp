e23930786b5205990139c81e6490080f18d895e4
DooxFonggg <phongcoi1235@gmail.com>
2023-05-08 21:40:09 +0700

    init

--- SCTDL070.cpp
+++ SCTDL070.cpp
@@ -1,42 +1,29 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<bits/stdc++.h>
+
 using namespace std;
 
 int main() {
-    int N, C;
-    cin >> N >> C;
-
-    vector<int> w(N);
-    int total_weight = 0;
-    for (int i = 0; i < N; i++) {
-        cin >> w[i];
-        total_weight += w[i];
-    }
-
-    int l = 0, r = total_weight, ans = 0;
-    while (l <= r) {
-        int mid = (l + r) / 2;
-        int sum = 0, cnt = 0;
-        for (int i = 0; i < N; i++) {
-            if (sum + w[i] <= mid) {
-                sum += w[i];
-            } else {
-                cnt++;
-                sum = w[i];
-            }
-        }
-        if (sum > 0) cnt++;
-
-        if (cnt > C) {
-            l = mid + 1;
-        } else {
-            ans = mid;
-            r = mid - 1;
-        }
-    }
-
-    cout << ans << endl;
-    return 0;
+	int t;
+	cin >> t;
+	while(t--) {
+		int c, n;
+		cin >> c >> n;
+		int a[n];
+		for(int i =0; i<n; i++) {
+			cin >> a[i];
+		}
+		sort(a, a+n);
+		int max1 = 0;
+		for(int i = 0; i<n; i++) {
+			int tong = 0;
+			int j = i;
+			// duyet theo thu tu tong nap lon nhat se dc update
+			while(j <n && tong + a[j] <= c) {
+				tong += a[j];
+				j++;
+			}
+			max1 =  max(max1, tong);
+		}
+		cout << max1 << endl;
+	}
 }
-
